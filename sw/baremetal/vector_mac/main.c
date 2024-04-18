3db2d1ea1bee785dd3467a0bd50c7bb744ba4c50
Aleksandar <aleksandarlilic03@gmail.com>
2024-04-18 09:27:39 +0200

    test(baremetal): change c_test to vector_mac; update vector_mac test to use byte instead of int

--- sw/baremetal/vector_mac/main.c
+++ sw/baremetal/vector_mac/main.c
@@ -1,15 +1,18 @@
-#define LOOP_COUNT 100000
+#define LOOP_COUNT 100000u
 
 void fail();
 void pass();
 
-const unsigned int ref[16] = {116, 131, 144, 155, 164, 171, 176, 179, 180, 179, 176, 171, 164, 155, 144, 131};
+const unsigned char ref[16] = {
+    116, 131, 144, 155, 164, 171, 176, 179, 
+    180, 179, 176, 171, 164, 155, 144, 131
+};
 
-volatile unsigned int a[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-volatile unsigned int b[16] = {16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-volatile unsigned int c[16];
+unsigned char a[16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+unsigned char b[16] = { 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+unsigned char c[16];
 
-int asm_add(unsigned int a, unsigned int b) {
+char asm_add(unsigned char a, unsigned char b) {
     asm volatile("add %0, %1, %2" 
                  : "=r"(a)
                  : "r"(a), "r"(b));
@@ -17,20 +20,20 @@ int asm_add(unsigned int a, unsigned int b) {
 }
 
 void set_c() {
-    for (int i = 0; i < 16; i++) {
+    for (char i = 0; i < 16; i++) {
         c[i] = 100 + i;
     }
 }
 
 void main(void) {
-    for (int i = 0; i < LOOP_COUNT; i++) {
+    for (unsigned int i = 0; i < LOOP_COUNT; i++) {
         set_c();
         
-        for (int j = 0; j < 16; j++) {
+        for (char j = 0; j < 16; j++) {
             c[j] = asm_add(c[j], a[j] * b[j]);
         }
 
-        for (int j = 0; j < 16; j++) {
+        for (char j = 0; j < 16; j++) {
             if (c[j] != ref[j]) {
                 asm volatile("add x29, x0, %0"
                              :
