5fb78d2469ddedd7175203a6400fbae2bf0c3c4a
Aleksandar <aleksandarlilic03@gmail.com>
2024-04-28 08:07:11 +0200

    perf(profiling): simplify instruction maps

--- script/analyze_profiling_log.py
+++ script/analyze_profiling_log.py
@@ -5,94 +5,46 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-inst_types = { 
-    "alu_t" : "ARITH",
-    "mem_t" : "MEM",
-    "branch_t" : "BRANCH",
-    "jump_t" : "JUMP",
-    "csr_t" : "CSR",
-    "env_t" : "ENV",
-    "lui_t" : "ARITH",
-    "auipc_t" : "ARITH",
-    "nop_t" : "NOP",
-    "fence_t" : "FENCE",
-    "mem_s_t" : "MEM_S",
-    "mem_l_t" : "MEM_L",
+PROFILER_ENDS = "_inst_profiler.json"
+MEM_S = "MEM_S"
+MEM_L = "MEM_L"
+
+inst_t_mem = {
+    MEM_S: ["sb", "sh", "sw"],
+    MEM_L: ["lb", "lh", "lw", "lbu", "lhu"],
 }
 
-inst_map = {
-    "add": inst_types["alu_t"],
-    "sub": inst_types["alu_t"],
-    "sll": inst_types["alu_t"],
-    "srl": inst_types["alu_t"],
-    "sra": inst_types["alu_t"],
-    "slt": inst_types["alu_t"],
-    "sltu": inst_types["alu_t"],
-    "xor": inst_types["alu_t"],
-    "or": inst_types["alu_t"],
-    "and": inst_types["alu_t"],
-    "nop": inst_types["nop_t"],
-    "addi": inst_types["alu_t"],
-    "slli": inst_types["alu_t"],
-    "srli": inst_types["alu_t"],
-    "srai": inst_types["alu_t"],
-    "slti": inst_types["alu_t"],
-    "sltiu": inst_types["alu_t"],
-    "xori": inst_types["alu_t"],
-    "ori": inst_types["alu_t"],
-    "andi": inst_types["alu_t"],
-    "lb": inst_types["mem_t"],
-    "lh": inst_types["mem_t"],
-    "lw": inst_types["mem_t"],
-    "lbu": inst_types["mem_t"],
-    "lhu": inst_types["mem_t"],
-    "sb": inst_types["mem_t"],
-    "sh": inst_types["mem_t"],
-    "sw": inst_types["mem_t"],
-    "fence.i": inst_types["fence_t"],
-    "lui": inst_types["lui_t"],
-    "auipc": inst_types["auipc_t"],
-    "ecall": inst_types["env_t"],
-    "ebreak": inst_types["env_t"],
-    "csrrw": inst_types["csr_t"],
-    "csrrs": inst_types["csr_t"],
-    "csrrc": inst_types["csr_t"],
-    "csrrwi": inst_types["csr_t"],
-    "csrrsi": inst_types["csr_t"],
-    "csrrci": inst_types["csr_t"],
-    "beq": inst_types["branch_t"],
-    "bne": inst_types["branch_t"],
-    "blt": inst_types["branch_t"],
-    "bge": inst_types["branch_t"],
-    "bltu": inst_types["branch_t"],
-    "bgeu": inst_types["branch_t"],
-    "jalr": inst_types["jump_t"],
-    "jal": inst_types["jump_t"],
+inst_t = {
+    "ARITH": ["add", "sub", "sll", "srl", "sra", "slt", "sltu", "xor", "or", "and", 
+              "addi", "slli", "srli", "srai", "slti", "sltiu", "xori", "ori", "andi",
+              "lui", "auipc"],
+    "MEM": inst_t_mem[MEM_S] + inst_t_mem[MEM_L],
+    "BRANCH": ["beq", "bne", "blt", "bge", "bltu", "bgeu"],
+    "JUMP": ["jalr", "jal"],
+    "CSR": ["csrrw", "csrrs", "csrrc", "csrrwi", "csrrsi", "csrrci"],
+    "ENV": ["ecall", "ebreak"],
+    "NOP": ["nop"],
+    "FENCE": ["fence.i"],
 }
 
-inst_mem_map = {
-    "lb": inst_types["mem_l_t"],
-    "lh": inst_types["mem_l_t"],
-    "lw": inst_types["mem_l_t"],
-    "lbu": inst_types["mem_l_t"],
-    "lhu": inst_types["mem_l_t"],
-    "sb": inst_types["mem_s_t"],
-    "sh": inst_types["mem_s_t"],
-    "sw": inst_types["mem_s_t"],
-}
+# create a reverse map for instruction types
+inst_t_map = {}
+for k, v in inst_t.items():
+    for inst in v:
+        inst_t_map[inst] = k
 
-inst_mem_name = {
-    inst_types["mem_l_t"] : "Load",
-    inst_types["mem_s_t"] : "Store"
-}
+inst_t_mem_map = {}
+for k, v in inst_t_mem.items():
+    for inst in v:
+        inst_t_mem_map[inst] = k
 
+# TODO: add function calls/graphs highlight when supported by the profiler
 colors = {
     "blue_base": "#0077b6",
     "blue_light1": "#00b4d8",
     "blue_light2": "#90e0ef",
 }
 
-# TODO: function calls/graphs when supported by the profiler
 highlight_colors = [
     "#2a9d8f",
     "#e9c46a",
@@ -100,7 +52,11 @@ highlight_colors = [
     "#e76f51",
 ]
 
-PROFILER_ENDS = "_inst_profiler.json"
+# memory instructions breakdown store vs load
+inst_mem_bd = {
+    MEM_L : ["Load", colors["blue_light1"]],
+    MEM_S : ["Store", colors["blue_light2"]],
+}
 
 def find_gs(x):
     prec = 4
@@ -120,6 +76,8 @@ def parse_args():
     parser.add_argument('-t', '--top', type=int, help="Number of N most common instructions to display. Default is all.")
     #parser.add_argument('-e', '--exclude', type=str, nargs='+', help="Exclude specific instructions")
     #parser.add_argument('--exclude_type', type=str, nargs='+', help="Exclude specific instruction types")
+    #parser.add_argument('-i', '--include', type=str, nargs='+', help="Include only specific instructions")
+    #parser.add_argument('--include_type', type=str, nargs='+', help="Include only specific instruction types")
     parser.add_argument('--highlight', '--hi', type=str, nargs='+', help="Highlight specific instructions")
     parser.add_argument('--silent', action='store_true', help="Don't display the chart(s) in pop-up window")
     parser.add_argument('--save_png', action='store_true', help="Save charts as PNG")
@@ -128,6 +86,7 @@ def parse_args():
     parser.add_argument('--combined_only', action='store_true', help="Only save combined charts/data. Ignored if single json file is provided")
     #parser.add_argument('-o', '--output', type=str, help="Output directory for the combined charts/data. Ignored if single json file is provided. Default is current directory. Standalone charts/data will be saved in the same directory as the source json file.")
     parser.add_argument('--allow_zero', action='store_true', default=False, help="Allow instructions with zero count to be displayed")
+    #parser.add_arument('--estimate_perf', type=str, help="Estimate performance based on instruction count. Requires JSON file with HW performance metrics")
     
     return parser.parse_args()
 
@@ -135,13 +94,13 @@ def analyze_log(df, hl_groups, log, args, combined=False):
     title = os.path.basename(log.replace(PROFILER_ENDS, ""))
 
     # separate the instructions by type
-    df['type'] = df['name'].map(inst_map)
-    df_g = df[['type', 'count']].groupby('type').sum()
+    df['i_type'] = df['name'].map(inst_t_map)
+    df_g = df[['i_type', 'count']].groupby('i_type').sum()
     df_g = df_g.sort_values(by='count', ascending=True)
 
     # separate the memory instructions by type
-    df['mem_type'] = df['name'].map(inst_mem_map)
-    df_mem_g = df[['mem_type', 'count']].groupby('mem_type').sum()
+    df['i_mem_type'] = df['name'].map(inst_t_mem_map)
+    df_mem_g = df[['i_mem_type', 'count']].groupby('i_mem_type').sum()
     df_mem_g = df_mem_g.sort_values(by='count', ascending=False)
 
     if args.top:
@@ -157,7 +116,10 @@ def analyze_log(df, hl_groups, log, args, combined=False):
                            figsize=(COLS*10, ROWS*(len(df)+len(df_g))/6),
                            height_ratios=(len(df)*.95, len(df_g)),
                            gridspec_kw=find_gs(len(df)+len(df_g)))
-    fig.suptitle(f'Execution profile for\n{title}\nInstructions profiled: {df["count"].sum()}')
+    suptitle_str = f"Execution profile for {title}"
+    suptitle_str += f"\nInstructions profiled: {df['count'].sum()}"
+    #suptitle_str += f"\nEstimated HW perf: ..."
+    fig.suptitle(suptitle_str)
     rect.append(ax[0].barh(df['name'], df['count'], color=colors["blue_base"]))
     rect.append(ax[1].barh(df_g.index, df_g['count'], color=colors["blue_base"]))
     y_ax0_offset = min(0.025, len(df)/2_000)
@@ -178,19 +140,14 @@ def analyze_log(df, hl_groups, log, args, combined=False):
                 r.set_color(highlight_colors[hc])
         hc += 1
     
-    # memory instructions breakdown store vs load
-    mem_color = {
-        inst_types["mem_l_t"] : colors["blue_light1"],
-        inst_types["mem_s_t"] : colors["blue_light2"]
-    }
-
     df_mem_g = df_mem_g[df_mem_g['count'] != 0] # never label if count is zero
     if len(df_mem_g) > 0:
-        mem_type_index = df_g.index.get_loc(inst_types["mem_t"])
+        mem_type_index = df_g.index.get_loc("MEM")
         left_start = 0
         for i, row in df_mem_g.iterrows():
             rect_m = ax[1].barh(mem_type_index, row['count'], left=left_start, 
-                                label=inst_mem_name[row.name], color=mem_color[row.name])
+                                label=inst_mem_bd[row.name][0], 
+                                color=inst_mem_bd[row.name][1])
             ax[1].bar_label(rect_m, padding=0, label_type='center', size=7)
             left_start += row['count']
 
@@ -257,13 +214,13 @@ def main():
     # combine all the data
     if len(all_logs) > 1:
         df_combined = pd.concat(df_arr)
-        if 'type' in df_combined.columns:
-            df_combined = df_combined.drop(columns=['type'])
-        if 'mem_type' in df_combined.columns:
-            df_combined = df_combined.drop(columns=['mem_type'])
+        if 'i_type' in df_combined.columns:
+            df_combined = df_combined.drop(columns=['i_type'])
+        if 'i_mem_type' in df_combined.columns:
+            df_combined = df_combined.drop(columns=['i_mem_type'])
         df_combined = df_combined.groupby('name', as_index=False).sum()
         df_combined = df_combined.sort_values(by='count', ascending=True)
-        analyze_log(df_combined, hl_groups, f"combined{PROFILER_ENDS}", args, combined=True)
+        analyze_log(df_combined, hl_groups, f"all workloads", args, combined=True)
 
 if __name__ == "__main__":
     main()
