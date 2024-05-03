b0ee03101b87b311b927cd74a045b4fcf86b06dc
Aleksandar <aleksandarlilic03@gmail.com>
2024-05-03 16:34:07 +0200

    test(baremetal): add prime numbers test; update pass/fail macro define order

--- sw/baremetal/defines/defines.h
+++ sw/baremetal/defines/defines.h
@@ -2,11 +2,12 @@
 // Pass/Fail Macro
 //-----------------------------------------------------------------------
 
+#define TESTNUM x28
+
 #define RVTEST_PASS                                                     \
         li TESTNUM, 1;                                                  \
         TEST_END
 
-#define TESTNUM x28
 #define RVTEST_FAIL                                                     \
         sll TESTNUM, TESTNUM, 1;                                        \
         or TESTNUM, TESTNUM, 1;                                         \
---
+++ sw/baremetal/prime_numbers/main.c
@@ -0,0 +1,50 @@
+#define MAX_LIMIT 10000
+//#define EXPECTED_PRIME_COUNT 4 // for n = 10
+//#define EXPECTED_PRIME_COUNT 25 // for n = 100
+#define EXPECTED_PRIME_COUNT 430 // for n = 3000
+
+void fail();
+void pass();
+
+// Static memory allocation
+volatile char prime[MAX_LIMIT + 1];
+
+void sieveOfEratosthenes(volatile int n) {
+    asm(".global set_defaults");
+    asm("set_defaults:");
+    unsigned int prime_count = 0;
+    for (int i = 2; i <= n; i++)
+        prime[i] = 1;
+
+    asm(".global find_primes");
+    asm("find_primes:");
+    for (int p = 2; p * p <= n; p++)
+        if (prime[p] == 1)
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = 0;
+
+    asm(".global count_primes");
+    asm("count_primes:");
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            prime_count++;
+
+    asm volatile("add x29, x0, %0"
+                  :
+                  : "r"(EXPECTED_PRIME_COUNT));
+    asm volatile("add x30, x0, %0"
+                  :
+                  : "r"(prime_count));
+    if (prime_count != EXPECTED_PRIME_COUNT){
+        asm volatile("addi x28, x0, 1"
+                      :
+                      : );
+        fail();
+    }
+}
+
+int main() {
+    int n = 3000;
+    sieveOfEratosthenes(n);
+    pass();
+}
