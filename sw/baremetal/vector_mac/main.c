fd5deffb59c4e776f9a37bfc7aa41367cb98fa06
Aleksandar <aleksandarlilic03@gmail.com>
2024-05-03 19:51:45 +0200

    test(baremetal): update vector mac test to use the entire input range for a and b

--- sw/baremetal/vector_mac/main.c
+++ sw/baremetal/vector_mac/main.c
@@ -1,18 +1,20 @@
-#define LOOP_COUNT 100000u
+#define LOOP_COUNT 1000u
 
 void fail();
 void pass();
 
-const unsigned char ref[16] = {
-    116, 131, 144, 155, 164, 171, 176, 179, 
-    180, 179, 176, 171, 164, 155, 144, 131
+unsigned char a[16] = {
+    99, 228, 216, 184, 167, 203, 173, 107, 177, 205, 12, 128, 51, 95, 222, 212};
+unsigned char b[16] = {
+    152, 237, 93, 250, 200, 154, 185, 76, 1, 93, 58, 80, 88, 124, 125, 251};
+unsigned int c[16] = {0};
+
+const unsigned int ref[16] = {
+    963072, 3458304, 1285632, 2944000, 2137600, 2000768, 2048320, 520448,
+    11328, 1220160, 44544, 655360, 287232, 753920, 1776000, 3405568
 };
 
-unsigned char a[16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-unsigned char b[16] = { 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-unsigned char c[16];
-
-char asm_add(unsigned char a, unsigned char b) {
+int asm_add(unsigned int a, unsigned int b) {
     asm volatile("add %0, %1, %2" 
                  : "=r"(a)
                  : "r"(a), "r"(b));
@@ -21,33 +23,37 @@ char asm_add(unsigned char a, unsigned char b) {
 
 void set_c() {
     for (char i = 0; i < 16; i++) {
-        c[i] = 100 + i;
+        c[i] = 0;
     }
+}
+
+void write_mismatch(unsigned int c, unsigned int ref, char j) {
+    asm volatile("add x29, x0, %0"
+                 :
+                 : "r"(c));
+    asm volatile("add x30, x0, %0"
+                 :
+                 : "r"(ref));
+    asm volatile("add x28, x0, %0"
+                 :
+                 : "r"(j+1)); // so that 0th index is 1st
 }
 
 void main(void) {
     for (unsigned int i = 0; i < LOOP_COUNT; i++) {
         set_c();
-        
-        for (char j = 0; j < 16; j++) {
-            c[j] = asm_add(c[j], a[j] * b[j]);
+        for (char j = 0; j < 64; j++) {
+            for (char k = 0; k < 16; k++) {
+                c[k] = asm_add(c[k], a[k] * b[k]);
+            }
         }
 
         for (char j = 0; j < 16; j++) {
             if (c[j] != ref[j]) {
-                asm volatile("add x29, x0, %0"
-                             :
-                             : "r"(c[j]));
-                asm volatile("add x30, x0, %0"
-                             :
-                             : "r"(ref[j]));
-                asm volatile("add x28, x0, %0"
-                             :
-                             : "r"(j+1)); // so that 0th index is 1st
+                write_mismatch(c[j], ref[j], j);
                 fail();
             }
         }
     }
     pass();
-    asm volatile("ecall");
 }
