413e52b64b2da99ba63f17ae2c7e50682a4c8cdb
Aleksandar <aleksandarlilic03@gmail.com>
2024-05-15 09:47:30 +0200

    dev: change write pattern for memory to A5

--- src/dev.h
+++ src/dev.h
@@ -7,7 +7,7 @@ class dev {
         std::vector<uint8_t> mem;
     public:
         dev() = delete;
-        dev(size_t size) : mem(size) {}
+        dev(size_t size) : mem(size) { std::fill(mem.begin(), mem.end(), 0xA5);}
         virtual uint8_t rd(uint32_t address) { return mem[address]; }
         virtual void wr(uint32_t address, uint8_t data) { mem[address] = data; }
 };
