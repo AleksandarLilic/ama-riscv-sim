d0266d4744a366197bd8c9b96c725aa45c4863d8
Aleksandar <aleksandarlilic03@gmail.com>
2024-05-28 09:22:31 +0200

    test(gtest): add tests to check for empty arg, not found bin file, and oversized bin file

--- test/test.cpp
+++ test/test.cpp
@@ -3,35 +3,58 @@
 #include <fstream>
 #include <vector>
 #include <filesystem>
+#include <stdexcept>
 #include <gtest/gtest.h>
 
+#include "../src/defines.h"
+
 #define CHECK_PASS "0x0340 (mscratch): 0x1"
+#define SIM_BIN "../src/ama-riscv-sim "
+
+struct cmd_setup {
+    std::string log_name;
+    std::string sim_cmd;
+};
 
 class sim_test : public ::testing::TestWithParam<std::string> {
-    protected:
-        bool check_test_result(const std::string &test_path) {
-            std::string sim_bin = "../src/ama-riscv-sim";
-            std::string log_name = gen_log_name(test_path) + "_dump.log";
-            std::string sim_cmd = sim_bin + " " + test_path + " > " + log_name;
-            int test_result = system(sim_cmd.c_str());
-            EXPECT_EQ(test_result, 0) << "Failed to run: <" << test_path \
-                << "> with error code: " << test_result;
-            
-            // open output log file and check result
+    private:
+        cmd_setup setup(const std::string &test_path) {
+            cmd_setup cs;
+            cs.log_name = gen_log_name(test_path) + "_dump.log";
+            cs.sim_cmd = SIM_BIN + test_path + " > " + cs.log_name + " 2>&1";
+            return cs;
+        }
+
+        bool find_str(const std::string &log_name, const std::string &str) {
             std::ifstream log_file(log_name);
             std::string line;
-            bool test_passed = false;
-            while (std::getline(log_file, line)) {
-                if (line.find(CHECK_PASS) != std::string::npos) {
-                    test_passed = true;
-                    break;
-                }
-            }
-            
+            while (std::getline(log_file, line))
+                if (line.find(str) != std::string::npos)
+                    return true;
+            return false;
+        }
+
+    protected:
+        bool check_test_result(const std::string &test_path) {
+            cmd_setup cs = setup(test_path);
+            int test_result = system(cs.sim_cmd.c_str());
+            EXPECT_EQ(test_result, 0) << "Failed to run: <" << test_path \
+                << "> with error code: " << test_result;
+
+            // open output log file and check result
+            bool test_passed = find_str(cs.log_name, CHECK_PASS);
             EXPECT_TRUE(test_passed) << "Test failed: <" << test_path << ">";
             return true;
         }
-    
+
+        bool check_exception(const std::string &test_path,
+                             const std::string &error_msg) {
+            cmd_setup cs = setup(test_path);
+            int test_result = system(cs.sim_cmd.c_str());
+            EXPECT_NE(test_result, 0) << "Expected error code: " << error_msg;
+            return find_str(cs.log_name, error_msg);
+        }
+
     public:
         static std::string gen_log_name(const std::string& path) {
             std::filesystem::path p(path);
@@ -70,6 +93,25 @@ INSTANTIATE_TEST_SUITE_P(
     Default,
     sim_test,
     // FIXME: take this from cli
-    ::testing::ValuesIn(read_testlist("gtest_testlist.txt")), 
+    ::testing::ValuesIn(read_testlist("gtest_testlist.txt")),
     test_name()
 );
+
+TEST_F(sim_test, missing_arguments) {
+    ASSERT_TRUE(check_exception("", "Missing arguments."));
+}
+
+TEST_F(sim_test, bin_file_not_found) {
+    ASSERT_TRUE(check_exception("not_found", "BIN failed to open."));
+}
+
+TEST_F(sim_test, bin_file_oversized) {
+    // generate dummy bin file larger than MEM_SIZE
+    std::ofstream test_bin("oversized.bin", std::ios::binary);
+    for (int i = 0; i <= MEM_SIZE; i++)
+        test_bin << "0";
+    test_bin.close();
+    ASSERT_TRUE(check_exception("oversized.bin",
+                                "File size is greater than memory size.")
+    );
+}
