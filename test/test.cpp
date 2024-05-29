9be9648ba5a3da4c870a97d40e9e8563a116e2f8
Aleksandar <aleksandarlilic03@gmail.com>
2024-05-29 09:08:47 +0200

    test(gtest): add tests to check unsupported instructions

--- test/test.cpp
+++ test/test.cpp
@@ -47,8 +47,8 @@ class sim_test : public ::testing::TestWithParam<std::string> {
             return true;
         }
 
-        bool check_exception(const std::string &test_path,
-                             const std::string &error_msg) {
+        bool check_error(const std::string &test_path,
+                         const std::string &error_msg) {
             cmd_setup cs = setup(test_path);
             int test_result = system(cs.sim_cmd.c_str());
             EXPECT_NE(test_result, 0) << "Expected error code: " << error_msg;
@@ -98,11 +98,11 @@ INSTANTIATE_TEST_SUITE_P(
 );
 
 TEST_F(sim_test, missing_arguments) {
-    ASSERT_TRUE(check_exception("", "Missing arguments."));
+    ASSERT_TRUE(check_error("", "Missing arguments."));
 }
 
 TEST_F(sim_test, bin_file_not_found) {
-    ASSERT_TRUE(check_exception("not_found", "BIN failed to open."));
+    ASSERT_TRUE(check_error("not_found", "BIN failed to open."));
 }
 
 TEST_F(sim_test, bin_file_oversized) {
@@ -111,7 +111,99 @@ TEST_F(sim_test, bin_file_oversized) {
     for (int i = 0; i <= MEM_SIZE; i++)
         test_bin << "0";
     test_bin.close();
-    ASSERT_TRUE(check_exception("oversized.bin",
-                                "File size is greater than memory size.")
+    ASSERT_TRUE(check_error("oversized.bin",
+                            "File size is greater than memory size.")
     );
+}
+
+#define UI_MSG(x) \
+    "Unsupported instruction: <" x "> 8000000c: " + gen_hex_str(arr[3])
+
+uint32_t arr[4] = {
+    0x00004117, //auipc  x2,0x4
+    0x00010113, //addi   x2,x2,0
+    0x00001197  //auipc  x3,0x1
+};
+
+void write_bin(const std::string &filename) {
+    std::ofstream test_bin(filename, std::ios::binary);
+    test_bin.write(reinterpret_cast<char*>(arr), sizeof(arr));
+    test_bin.close();
+}
+
+std::string gen_hex_str(uint32_t val) {
+    char hex_str[9];
+    std::sprintf(hex_str, "%08x", val);
+    return std::string(hex_str);
+}
+
+// TODO: can these be parametrized with the existing class?
+TEST_F(sim_test, unsupported_opcode) {
+    arr[3] = 0x0000007f;
+    std::string filename = "unsupported_opcode.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("opcode")));
+}
+
+TEST_F(sim_test, unsupported_al_reg) {
+    arr[3] = 0x40003033;
+    std::string filename = "unsupported_al_reg.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("al_reg")));
+}
+
+TEST_F(sim_test, unsupported_al_imm) {
+    arr[3] = 0x40001013;
+    std::string filename = "unsupported_al_imm.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("al_imm")));
+}
+
+TEST_F(sim_test, unsupported_load) {
+    arr[3] = 0x00003003;
+    std::string filename = "unsupported_load.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("load")));
+}
+
+TEST_F(sim_test, unsupported_store) {
+    arr[3] = 0x00007023;
+    std::string filename = "unsupported_store.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("store")));
+}
+
+TEST_F(sim_test, unsupported_branch) {
+    arr[3] = 0x00002063;
+    std::string filename = "unsupported_branch.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("branch")));
+}
+
+TEST_F(sim_test, unsupported_jalr) {
+    arr[3] = 0x00002067;
+    std::string filename = "unsupported_jalr.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("jalr")));
+}
+
+TEST_F(sim_test, unsupported_sys) {
+    arr[3] = 0x34004073;
+    std::string filename = "unsupported_sys.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("sys")));
+}
+
+TEST_F(sim_test, unsupported_csr) {
+    arr[3] = 0xfff05073;
+    std::string filename = "unsupported_csr.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, "Unsupported CSR. Address: 0xfff"));
+}
+
+TEST_F(sim_test, unsupported_misc_mem) {
+    arr[3] = 0x0000200f;
+    std::string filename = "unsupported_misc_mem.bin";
+    write_bin(filename);
+    ASSERT_TRUE(check_error(filename, UI_MSG("misc_mem")));
 }
