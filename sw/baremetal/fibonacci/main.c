f5237693e53547ffa5a1e1005739e00f9a2df331
Aleksandar <aleksandarlilic03@gmail.com>
2025-01-06 19:51:21 +0100

    sw(baremetal): add linear fibonacci search (recursive still default) and add more reference values

--- sw/baremetal/fibonacci/main.c
+++ sw/baremetal/fibonacci/main.c
@@ -9,16 +9,39 @@
 #define N_IN 10
 #endif
 
-#if (N_IN == 20)
+#if (N_IN == 40)
+#define SET_N volatile uint32_t n = 40;
+#define SET_EXP uint32_t expected = 102334155;
+#elif (N_IN == 30)
+#define SET_N volatile uint32_t n = 30;
+#define SET_EXP uint32_t expected = 832040;
+#elif (N_IN == 20)
 #define SET_N volatile uint32_t n = 20;
 #define SET_EXP uint32_t expected = 6765;
 #elif (N_IN == 18)
 #define SET_N volatile uint32_t n = 18;
 #define SET_EXP uint32_t expected = 2584;
+#elif (N_IN == 15)
+#define SET_N volatile uint32_t n = 15;
+#define SET_EXP uint32_t expected = 610;
 #elif (N_IN == 10)
 #define SET_N volatile uint32_t n = 10;
 #define SET_EXP uint32_t expected = 55;
 #endif
+
+/*
+uint32_t fib(uint32_t n) {
+    uint32_t a = 0;
+    uint32_t b = 1;
+    uint32_t tmp;
+    while (n-- > 0) {
+        tmp = a;
+        a = b;
+        b += tmp;
+    }
+    return a;
+}
+*/
 
 uint32_t fib(uint32_t n) {
     if (n <= 2)
