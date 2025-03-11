7a3296a5b1cff3a32abb3cb461f52ce9ffd6b575
Aleksandar <aleksandarlilic03@gmail.com>
2025-03-11 09:45:24 +0100

    perf(profiling): add options to save outputs from register file usage plot script

--- script/plot_rf_usage.py
+++ script/plot_rf_usage.py
@@ -2,14 +2,18 @@
 
 import os
 import sys
+import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-log = sys.argv[1]
+parser = argparse.ArgumentParser(description="Plot register file usage")
+parser.add_argument('log', help="Input binary log from profiler")
+parser.add_argument('--save_png', action='store_true', help="Save charts as PNG")
+parser.add_argument('--save_svg', action='store_true', help="Save charts as SVG")
+parser.add_argument('--save_csv', action='store_true', help="Save source data formatted as CSV")
 
-if not os.path.isfile(log):
-    raise ValueError(f"File {log} does not exist")
+args = parser.parse_args()
 
 rf_names = [
     "zero","ra","sp","gp","tp","t0","t1","t2","s0","s1","a0","a1","a2","a3","a4","a5",
@@ -22,7 +26,7 @@ dtype = np.dtype([
     (h[1], np.uint32),
     (h[2], np.uint32),
 ])
-data = np.fromfile(log, dtype=dtype)
+data = np.fromfile(args.log, dtype=dtype)
 df = pd.DataFrame(data, columns=h)
 df['total'] = df.sum(axis=1)
 df['rs'] = df['rs1'] + df['rs2']
@@ -31,13 +35,14 @@ df.insert(0, 'reg', 'x' + df.index.astype(str))
 # add a new column with the register names using the index
 df['reg_name'] = df.index.map(lambda x: rf_names[x])
 df['reg_comb'] = df['reg_name'] + " (" + df['reg'] + ")"
-df.to_csv(log.replace('.bin', '.csv'), index=False)
+if args.save_csv:
+    df.to_csv(args.log.replace('.bin', '.csv'), index=False)
 
 columns = [['rd', 'rs1', 'rs2'], ['rd', 'rs'], ['total']]
 for col_set in columns:
-    fig, ax = plt.subplots(figsize=(15, 12))
+    fig, ax = plt.subplots(figsize=(12, 10), constrained_layout=True)
     bar_width = 0.7
-    alpha = 0.6
+    alpha = 0.55
     y_axis_numeric = np.arange(df.index.size)
     bars = []
     for idx, col in enumerate(col_set):
@@ -53,8 +58,14 @@ for col_set in columns:
     ax.set_yticks(y_axis_numeric)
     ax.set_yticklabels(df['reg_comb'])
     ax.set_xlabel('Count')
-    ax.set_title("Register Histogram\n\n")
+    ax.set_title("Register File usage\n\n")
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.045), ncol=8)
     ax.margins(y=0.01, x=0)
     ax.grid(axis='x', alpha=0.7)
     ax.axhspan(20-0.5, 31+0.5, facecolor='lightgray', alpha=0.6, zorder=0)
+
+    name = "_".join(col_set)
+    if args.save_png:
+        fig.savefig(args.log.replace(" ", "_").replace(".bin", f"_{name}.png"))
+    if args.save_svg:
+        fig.savefig(args.log.replace(" ", "_").replace(".bin", f"_{name}.svg"))
