AleksandarLilic/ama-riscv-sim

commit 61d43a3e24d8f666820690edee2994336fd4ad69

hw model(bp): add option to fold PC for indexing, and appropriate cli args

src/hw_models/bp_gselect.h
@@ -33,7 +33,7 @@ class bp_gselect : public bp {
         }
 
         virtual uint32_t get_idx(uint32_t pc, uint32_t gr) {
-            uint32_t pc_part = (pc >> BP_PC_CUTOFF_BITS) & pc_mask;
+            uint32_t pc_part = get_pc(pc, cnt.get_idx_mask());
             uint32_t gr_part = gr & gr_mask;
             // concat with pc top, gr bottom
             return ((pc_part << gr_bits) | gr_part) & idx_mask;

src/hw_models/bp_gshare.h
@@ -35,7 +35,7 @@ class bp_gshare : public bp {
         }
 
         virtual uint32_t get_idx(uint32_t pc, uint32_t gr) {
-            uint32_t pc_part = (pc >> BP_PC_CUTOFF_BITS) & pc_mask;
+            uint32_t pc_part = get_pc(pc, cnt.get_idx_mask());
             uint32_t gr_part = gr & gr_mask;
             // xor with top PC bits, fit within the mask length
             // TODO: slide XORs bits L/R?

src/hw_models/bp.h
@@ -11,27 +11,44 @@
 #endif
 
 struct bp_cfg_t {
-    const uint8_t pc_bits;
-    const uint8_t cnt_bits;
-    const uint8_t hist_bits;
-    const uint8_t gr_bits;
-    //const std::string type_name;
-    const char* type_name;
+    public:
+        const uint8_t pc_bits;
+        const uint8_t cnt_bits;
+        const uint8_t hist_bits;
+        const uint8_t gr_bits;
+        const bp_pc_folds_t fold_pc;
+        //const std::string type_name;
+        const char* type_name;
 };
 
 class bp {
     public:
         const std::string type_name;
         bp_cnt* cnt_ptr = nullptr;
+        const uint8_t pc_bits;
+        const bp_pc_folds_t fold_pc;
 
     protected:
         uint32_t predicted_pc;
         b_dir_t b_dir_last;
         uint32_t size;
         bp_stats_t stats;
+        uint8_t pc_folds;
 
     public:
-        bp(bp_cfg_t cfg) : type_name(cfg.type_name), stats(cfg.type_name) {}
+        bp(bp_cfg_t cfg) :
+            type_name(cfg.type_name),
+            pc_bits(cfg.pc_bits),
+            fold_pc(cfg.fold_pc),
+            stats(cfg.type_name)
+        {
+            pc_folds = 0;
+            // the most possible # of folds, without going past ADDR_BITS
+            if (fold_pc == bp_pc_folds_t::all) {
+                pc_folds = TO_U32((ADDR_BITS - BP_PC_CUTOFF_BITS) / pc_bits);
+                //pc_folds -= 1;
+            }
+        }
 
         virtual ~bp() = default;
 
@@ -40,6 +57,14 @@ class bp {
         virtual uint32_t predict(uint32_t target_pc, uint32_t pc) = 0;
 
         virtual bool eval_and_update(bool taken, uint32_t next_pc) = 0;
+
+        virtual uint32_t get_pc(uint32_t pc, uint32_t mask) {
+            uint32_t pc_part = 0;
+            for (size_t p = 0; p <= pc_folds; p++) {
+                pc_part ^= (pc >> (BP_PC_CUTOFF_BITS + (pc_bits * p))) & mask;
+            }
+            return pc_part;
+        }
 
         virtual void update_stats(uint32_t pc, uint32_t next_pc) {
             bool correct = (next_pc == predicted_pc);

src/hw_models/bp_bimodal.h
@@ -26,9 +26,7 @@ class bp_bimodal : public bp {
             cnt_ptr = &cnt;
         }
 
-        uint32_t get_idx(uint32_t pc) {
-            return (pc >> BP_PC_CUTOFF_BITS) & cnt.get_mask();
-        }
+        uint32_t get_idx(uint32_t pc) { return get_pc(pc, cnt.get_idx_mask()); }
 
         virtual uint32_t predict(uint32_t target_pc, uint32_t pc) override {
             idx_last = get_idx(pc);

src/hw_models/bp_cnt.h
@@ -73,7 +73,7 @@ class bp_cnt {
         }
 
         uint32_t get_bit_size() { return bit_size; }
-        uint32_t get_mask() { return cnt_entries_mask; }
+        uint32_t get_idx_mask() { return cnt_entries_mask; }
 
         bool thr_check(uint32_t idx) { return cnt_table[idx] >= thr_taken; }
 

src/hw_models/bp_combined.h
@@ -30,9 +30,7 @@ class bp_combined : public bp {
 
         void add_size(uint32_t size) { this->size += size; }
 
-        uint32_t get_idx(uint32_t pc) {
-            return (pc >> BP_PC_CUTOFF_BITS) & cnt.get_mask();
-        }
+        uint32_t get_idx(uint32_t pc) { return get_pc(pc, cnt.get_idx_mask()); }
 
         uint32_t select(uint32_t pc) {
             idx_last = get_idx(pc);

src/hw_models/bp_if.cpp
@@ -1,29 +1,85 @@
 #include "bp_if.h"
 
-// { pc_bits, cnt_bits, hist_bits, gr_bits, type_name }
-#define BP_CFG_NONE { 0, 0, 0, 0, hw_cfg.bp_active_name.c_str() }
+// {
+//     pc_bits,
+//     cnt_bits,
+//     hist_bits,
+//     gr_bits,
+//     type_name
+// }
+
+#define BP_CFG_NONE { \
+    0, \
+    0, \
+    0, \
+    0, \
+    bp_pc_folds_t::none, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 // reuse cnt bits as method for static
 #define BP_CFG_STATIC { \
-    0, TO_U8(hw_cfg.bp_static_method), 0, 0, \
-    hw_cfg.bp_active_name.c_str() }
+    0, \
+    TO_U8(hw_cfg.bp_static_method), \
+    0, \
+    0, \
+    bp_pc_folds_t::none, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 #define BP_BIMODAL_CFG { \
-    hw_cfg.bp_pc_bits, hw_cfg.bp_cnt_bits, 0, 0, \
-    hw_cfg.bp_active_name.c_str() }
+    hw_cfg.bp_pc_bits, \
+    hw_cfg.bp_cnt_bits, \
+    0, \
+    0, \
+    hw_cfg.bp_fold_pc, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 #define BP_LOCAL_CFG { \
-    hw_cfg.bp_pc_bits, hw_cfg.bp_cnt_bits, hw_cfg.bp_lhist_bits, 0, \
-    hw_cfg.bp_active_name.c_str() }
+    hw_cfg.bp_pc_bits, \
+    hw_cfg.bp_cnt_bits, \
+    hw_cfg.bp_lhist_bits, \
+    0, \
+    hw_cfg.bp_fold_pc, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 #define BP_GLOBAL_CFG { \
-    0, hw_cfg.bp_cnt_bits, 0, hw_cfg.bp_gr_bits, \
-    hw_cfg.bp_active_name.c_str() }
+    0, \
+    hw_cfg.bp_cnt_bits, \
+    0, \
+    hw_cfg.bp_gr_bits, \
+    hw_cfg.bp_fold_pc, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 #define BP_GSELECT_CFG { \
-    hw_cfg.bp_pc_bits, hw_cfg.bp_cnt_bits, 0, hw_cfg.bp_gr_bits, \
-    hw_cfg.bp_active_name.c_str() }
+    hw_cfg.bp_pc_bits, \
+    hw_cfg.bp_cnt_bits, \
+    0, \
+    hw_cfg.bp_gr_bits, \
+    hw_cfg.bp_fold_pc, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 #define BP_GSHARE_CFG { \
-    hw_cfg.bp_pc_bits, hw_cfg.bp_cnt_bits, 0, hw_cfg.bp_gr_bits, \
-    hw_cfg.bp_active_name.c_str() }
+    hw_cfg.bp_pc_bits, \
+    hw_cfg.bp_cnt_bits, \
+    0, \
+    hw_cfg.bp_gr_bits, \
+    hw_cfg.bp_fold_pc, \
+    hw_cfg.bp_active_name.c_str() \
+}
+
 #define BP_COMBINED_CFG { \
-    hw_cfg.bp_combined_pc_bits, hw_cfg.bp_combined_cnt_bits, 0, 0, \
-    hw_cfg.bp_active_name.c_str() }
+    hw_cfg.bp_combined_pc_bits, \
+    hw_cfg.bp_combined_cnt_bits, \
+    0, \
+    0, \
+    hw_cfg.bp_combined_fold_pc, \
+    hw_cfg.bp_active_name.c_str() \
+}
 
 bp_if::bp_if(std::string name, hw_cfg_t hw_cfg) :
     bp_name(name),
@@ -86,6 +142,7 @@ std::unique_ptr<bp> bp_if::create_predictor(bp_t bp_type, hw_cfg_t hw_cfg) {
             hw_cfg.bp_cnt_bits = hw_cfg.bp2_cnt_bits;
             hw_cfg.bp_lhist_bits = hw_cfg.bp2_lhist_bits;
             hw_cfg.bp_gr_bits = hw_cfg.bp2_gr_bits;
+            hw_cfg.bp_fold_pc = hw_cfg.bp2_fold_pc;
             bp2 = create_predictor(bp_combined_p2_type, hw_cfg);
 
             bp_out = std::make_unique<

src/hw_models/bp_if.h
@@ -58,41 +58,59 @@ class bp_if {
 
     private:
         // predefined branch predictors
-        // { pc_bits, cnt_bits, hist_bits, gr_bits, type_name }
-        static constexpr std::array<bp_def_t, 15> arch_bp_defs = {{
-            {bp_t::sttc,
-             bp_cfg_t{ 0, TO_U8(bp_sttc_t::at), 0, 0, "d_static_at" }},
-            {bp_t::sttc,
-             bp_cfg_t{ 0, TO_U8(bp_sttc_t::ant), 0, 0, "d_static_ant" }},
-            {bp_t::sttc,
-             bp_cfg_t{ 0, TO_U8(bp_sttc_t::btfn), 0, 0, "d_static_btfn" }},
-            {bp_t::none, bp_cfg_t{ 0, 0, 0, 0, "d_none" }},
-            {bp_t::ideal, bp_cfg_t{ 0, 0, 0, 0, "d_ideal_" }},
-            {bp_t::bimodal, bp_cfg_t{ 7, 3, 0, 0, "d_bimodal_v1" }},
-            {bp_t::local, bp_cfg_t{ 5, 3, 5, 0, "d_local_v1" }},
-            {bp_t::global, bp_cfg_t{ 0, 3, 0, 7, "d_global_v1" }},
-            {bp_t::gselect, bp_cfg_t{ 1, 3, 0, 6, "d_gselect_v1" }},
-            {bp_t::gshare, bp_cfg_t{ 8, 1, 0, 8, "d_gshare_v1" }},
-            {bp_t::bimodal, bp_cfg_t{ 8, 3, 0, 0, "d_bimodal_v2" }},
-            {bp_t::local, bp_cfg_t{ 5, 3, 9, 0, "d_local_v2" }},
-            {bp_t::global, bp_cfg_t{ 0, 3, 0, 9, "d_global_v2" }},
-            {bp_t::gselect, bp_cfg_t{ 3, 3, 0, 6, "d_gselect_v2" }},
-            {bp_t::gshare, bp_cfg_t{ 8, 3, 0, 8, "d_gshare_v2" }},
+        // e.g. some good performing predictors that can be used as a starting
+        // point for further evaluation or quick comparison with the cli bp
+        // but without the need to go all out on the sweep flow
+        // more should be added as needed
+
+        #define FN bp_pc_folds_t::none
+        #define FA bp_pc_folds_t::all
+        #define STTC(x) TO_U8(bp_sttc_t::x)
+
+        inline static const
+        std::array<bp_def_t, 15> arch_bp_defs = {{
+            // {pc_bits, cnt_bits, hist_bits, gr_bits, pc_fold_bits, type_name}
+            // statics
+            {bp_t::sttc, bp_cfg_t{0, STTC(at), 0, 0, FN, "d_static_at"}},
+            {bp_t::sttc, bp_cfg_t{0, STTC(ant), 0, 0, FN, "d_static_ant"}},
+            {bp_t::sttc, bp_cfg_t{0, STTC(btfn), 0, 0, FN, "d_static_btfn"}},
+            // all or nothing
+            {bp_t::none, bp_cfg_t{0, 0, 0, 0, FN, "d_none"}},
+            {bp_t::ideal, bp_cfg_t{0, 0, 0, 0, FN, "d_ideal_"}},
+            // smaller
+            {bp_t::bimodal, bp_cfg_t{7, 3, 0, 0, FN, "d_bimodal_v1"}},
+            {bp_t::local, bp_cfg_t{5, 3, 5, 0, FN, "d_local_v1"}},
+            {bp_t::global, bp_cfg_t{0, 3, 0, 7, FN, "d_global_v1"}},
+            {bp_t::gselect, bp_cfg_t{1, 3, 0, 6, FN, "d_gselect_v1"}},
+            {bp_t::gshare, bp_cfg_t{8, 1, 0, 8, FN, "d_gshare_v1"}},
+            // a bit larger
+            {bp_t::bimodal, bp_cfg_t{8, 3, 0, 0, FN, "d_bimodal_v2"}},
+            {bp_t::local, bp_cfg_t{5, 3, 9, 0, FN, "d_local_v2"}},
+            {bp_t::global, bp_cfg_t{0, 3, 0, 9, FN, "d_global_v2"}},
+            {bp_t::gselect, bp_cfg_t{3, 3, 0, 6, FN, "d_gselect_v2"}},
+            {bp_t::gshare, bp_cfg_t{8, 3, 0, 8, FN, "d_gshare_v2"}},
         }};
 
-        static constexpr std::array<std::array<bp_def_t, 3>, 2> arch_bpc_defs =
+        inline static const
+        std::array<std::array<bp_def_t, 3>, 3> arch_bpc_defs = {{
+        // {pc_bits, cnt_bits, hist_bits, gr_bits, pc_fold_bits, type_name}
         {{
-            // { pc_bits, cnt_bits, hist_bits, gr_bits, type_name }
-            {{
-                {bp_t::combined, bp_cfg_t{ 4, 4, 0, 0, "d_combined_v1" }},
-                {bp_t::sttc, bp_cfg_t{ 0, 0, 0, 0, "d_static_cv1" }},
-                {bp_t::gshare, bp_cfg_t{ 8, 1, 0, 8, "d_gshare_cv1" }},
-            }},
-            {{
-                {bp_t::combined, bp_cfg_t{ 5, 4, 0, 0, "d_combined_v2" }},
-                {bp_t::sttc, bp_cfg_t{ 0, 0, 0, 0, "d_static_cv2" }},
-                {bp_t::gshare, bp_cfg_t{ 8, 3, 0, 8, "d_gshare_cv2" }},
-            }},
+            // combined predictor 1
+            {bp_t::combined, bp_cfg_t{4, 4, 0, 0, FN, "d_combined_v1"}},
+            {bp_t::sttc, bp_cfg_t{0, 0, 0, 0, FN, "d_static_cv1"}},
+            {bp_t::gshare, bp_cfg_t{8, 1, 0, 8, FN, "d_gshare_cv1"}},
+        }},
+        {{
+            // combined predictor 2
+            {bp_t::combined, bp_cfg_t{5, 4, 0, 0, FN, "d_combined_v2"}},
+            {bp_t::sttc, bp_cfg_t{0, 0, 0, 0, FN, "d_static_cv2"}},
+            {bp_t::gshare, bp_cfg_t{8, 3, 0, 8, FN, "d_gshare_cv2"}},
+        }},
+        {{
+            // combined predictor 3
+            {bp_t::combined, bp_cfg_t{4, 4, 0, 0, FN, "d_combined_v3"}},
+            {bp_t::sttc, bp_cfg_t{STTC(btfn), 0, 0, 0, FN, "d_static_cv3"}},
+            {bp_t::gselect, bp_cfg_t{2, 1, 0, 6, FN, "d_gselect_cv3"}},
+        }},
         }};
-
 };

commit 6f07e113ef6b6df6f557af39375b5ef1ab92bbd6

hw model(bp): fix PC masking bug for gshare and gselect introduced in 61d43a3 commit

src/hw_models/bp_gselect.h
@@ -33,7 +33,7 @@ class bp_gselect : public bp {
         }
 
         virtual uint32_t get_idx(uint32_t pc, uint32_t gr) {
-            uint32_t pc_part = get_pc(pc, cnt.get_idx_mask());
+            uint32_t pc_part = get_pc(pc, pc_mask);
             uint32_t gr_part = gr & gr_mask;
             // concat with pc top, gr bottom
             return ((pc_part << gr_bits) | gr_part) & idx_mask;

src/hw_models/bp_gshare.h
@@ -35,7 +35,7 @@ class bp_gshare : public bp {
         }
 
         virtual uint32_t get_idx(uint32_t pc, uint32_t gr) {
-            uint32_t pc_part = get_pc(pc, cnt.get_idx_mask());
+            uint32_t pc_part = get_pc(pc, pc_mask);
             uint32_t gr_part = gr & gr_mask;
             // xor with top PC bits, fit within the mask length
             // TODO: slide XORs bits L/R?