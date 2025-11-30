38751021f36e84fe2e4c8b8e26f1a6b11a02ce76
Aleksandar <aleksandarlilic03@gmail.com>
2025-11-30 07:16:53 +0100

    hw model(bp): fix for 1e6e721 that removed profiling() call for bp stats

--- src/hw_models/bp.h
+++ src/hw_models/bp.h
@@ -56,9 +56,7 @@ class bp {
             return pc_part;
         }
 
-        #ifdef DPI
         virtual void profiling(bool enable) { stats.profiling(enable); }
-        #endif
 
         virtual void update_stats(uint32_t pc, uint32_t next_pc) {
             bool correct = (next_pc == predicted_pc);
