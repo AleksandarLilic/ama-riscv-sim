a176df70792a69fadf8d557b1c8f43b6866f9787
Aleksandar <aleksandarlilic03@gmail.com>
2026-02-25 07:00:03 +0100

    sw(build): rename objdump output from .dump to .dasm

--- sw/baremetal/coremark/get_prof_range_coremark.sh
+++ sw/baremetal/coremark/get_prof_range_coremark.sh
@@ -3,10 +3,10 @@
 # gets one loop of coremark
 # get -4/+4 of the start/stop respectively to profile all loops
 
-#dump=$1
-dump="coremark.dump"
+#dasm=$1
+dasm="coremark.dasm"
 
-p_start=$(rg "iterate\.isra\.0\+0x2c" "$dump" | awk -F, '{print $NF}' | awk '{print $1}')
-p_stop=$(rg "iterate\.isra\.0\+0x2c" "$dump" | awk '{print $1}' | awk -F: '{print $1}')
+p_start=$(rg "iterate\.isra\.0\+0x2c" "$dasm" | awk -F, '{print $NF}' | awk '{print $1}')
+p_stop=$(rg "iterate\.isra\.0\+0x2c" "$dasm" | awk '{print $1}' | awk -F: '{print $1}')
 
 echo "--prof_pc_start $p_start --prof_pc_stop $p_stop"
--- sw/baremetal/dhrystone/get_prof_range_dhry.sh
+++ sw/baremetal/dhrystone/get_prof_range_dhry.sh
@@ -3,10 +3,10 @@
 # gets one loop of dhrystone
 # get -4/+4 of the start/stop respectively to profile all loops
 
-#dump=$1
-dump="dhrystone.dump"
+#dasm=$1
+dasm="dhrystone.dasm"
 
-p_start=$(rg "jal.*Proc_5" "$dump" | awk '{print $1}' | awk -F: '{print $1}')
-p_stop=$(rg "bne.*${p_start}" "$dump" | awk '{print $1}' | awk -F: '{print $1}')
+p_start=$(rg "jal.*Proc_5" "$dasm" | awk '{print $1}' | awk -F: '{print $1}')
+p_stop=$(rg "bne.*${p_start}" "$dasm" | awk '{print $1}' | awk -F: '{print $1}')
 
 echo "--prof_pc_start $p_start --prof_pc_stop $p_stop"
