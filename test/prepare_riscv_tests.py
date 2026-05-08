751e71c08f351a92cde5d5d5e39d1ce7807cc276
Aleksandar <aleksandarlilic03@gmail.com>
2026-05-08 08:01:03 +0200

    test(suite): fix codegen list

--- test/prepare_riscv_tests.py
+++ test/prepare_riscv_tests.py
@@ -17,7 +17,10 @@ TEST_DIR = os.path.join(GIT_ROOT, 'sw/baremetal')
 ISA_TEST_DIR = os.path.join(GIT_ROOT, 'sw/riscv-isa-tests')
 WORK_DIR = os.getcwd()
 
-CODEGEN_APPS = ["aapg", "sorting", "vector_ew_basic", "matmul", "memcpy"]
+CODEGEN_APPS = [
+    "aapg", "sorting", "vector_ew_basic", "matmul", "memcpy",
+    "dcache_ds_2", "dcache_ds_all"
+]
 
 def file_exists(filename):
     if not os.path.exists(filename):
