3e572f9bd98a8db129562cf002a700996cd9e200
Aleksandar <aleksandarlilic03@gmail.com>
2026-05-29 06:12:23 +0200

    sw(baremetal): add a few more examples to cpp_smoke test

--- sw/baremetal/cpp_smoke/main.cpp
+++ sw/baremetal/cpp_smoke/main.cpp
@@ -23,10 +23,6 @@ private:
     uint32_t value;
 };
 
-namespace res {
-    constexpr uint32_t expected = 305419914;
-}
-
 template <typename T, size_t N>
 constexpr T sum(const std::array<T, N>& a) {
     T acc = 0;
@@ -34,17 +30,47 @@ constexpr T sum(const std::array<T, N>& a) {
     return acc;
 }
 
+template <bool B>
+constexpr uint32_t branch_select(uint32_t val) {
+    if constexpr (B) {
+        return (val + 1);
+    } else {
+        static_assert(B, "dead branch instantiated");
+        return 0;
+    }
+}
+
+struct tda {
+    tda_cnt_t counters = {}; // optionally private if not accessed from outside
+    tda()  { init_tda_counters(); }
+    ~tda() { save_tda_counters(&counters); print_tda_counters_json(&counters); }
+};
+
+namespace res {
+    // acc(305419914) + lambda(+123) + branch_select(+1)
+    constexpr uint32_t expected = 305420038;
+}
+
 int main() {
-    constexpr std::array<int, 4> vals = {1, 2, 3, 4};
-    static_assert(sum(vals) == 10);
+    {
+        tda t;
+        constexpr std::array<int, 4> vals = {1, 2, 3, 4};
+        static_assert(sum(vals) == 10);
 
-    constexpr accumulator<0x12345678u> acc{0x10u}; // 305419880
-    volatile uint32_t in = 0x22u;
-    volatile uint32_t result = acc.apply(in, op::_add);
+        constexpr accumulator<0x12345678u> acc{0x10u}; // 305419880
+        volatile uint32_t in = 0x22u;
+        volatile uint32_t result = acc.apply(in, op::_add); // 305419914
 
-    if (res::expected != result) {
-        write_mismatch(result, res::expected, 1);
-        fail();
+        const uint32_t delta = 123u;
+        auto inc = [result](uint32_t x) { return x + result; };
+        result = inc(delta); // +123 = 305420037
+
+        result = branch_select<true>(result); // +1 = 305420038
+
+        if (res::expected != result) {
+            write_mismatch(result, res::expected, 1);
+            fail();
+        }
     }
 
     pass();
