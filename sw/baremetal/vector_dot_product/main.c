8724aafb92983972e14a14aee435fba80b857604
Aleksandar <aleksandarlilic03@gmail.com>
2026-07-23 07:16:38 +0200

    baremetal(vector dotp): add int2 flavors; use unaligned vectors for all cases

--- sw/baremetal/vector_dot_product/main.c
+++ sw/baremetal/vector_dot_product/main.c
@@ -7,21 +7,26 @@
 #endif
 
 #define ARR_LEN 16
+// packed data type has to within 1 byte exactly
 
 // input data A
-#if defined(NF_INT16) || defined(NF_INT16_INT8) || defined(NF_INT16_INT4)
+#if defined(NF_INT16) || defined(NF_INT16_INT8) || defined(NF_INT16_INT4) || defined(NF_INT16_INT2)
 int16_t a[ARR_LEN] = {
     -30036, 10799, 9845, 19648, 1, -11525, -2365, 1,
-    9225, 24275, 1, 1, 14116, -17833, -17338, 15832 };
+    9225, 24275, 1, 1, 14116, -17833, -17338, 15832
+};
 #elif defined(NF_INT8) || defined(NF_INT8_INT4) || defined(NF_INT8_INT2)
 int8_t a[ARR_LEN] = {
     44, -81, -11, 64, -61, 123, 67, -25, -119, 83, -107, 114, -92, -41, -58, 88
 };
-#elif defined(NF_INT4)
-// packed data type, has to be a multiple of 2 (pad with 0 at the end if needed)
+#elif defined(NF_INT4) || defined(NF_INT4_INT2)
 int8_t a[ARR_LEN>>1] = { 116, -115, 59, -5, -79, -83, -4, 14 };
 //actual values
 //{ (4, 7), (-3, -8), (-5, 3), (-5, -1), (1, -5), (-3, -6), (-4, -1), (-2, 0) };
+#elif defined(NF_INT2)
+int8_t a[ARR_LEN>>2] = {-74, 85, 55, -122};
+//actual values
+// { (-2, 1, -1, -2), (1, 1, 1, 1), -(1, 1, -1, 0), (-2, 1, 0, -2) };
 #else
 _Static_assert(0, "Unsupported number format: a");
 #endif
@@ -30,7 +35,8 @@ _Static_assert(0, "Unsupported number format: a");
 #if defined(NF_INT16)
 int16_t b[ARR_LEN] = {
     6744, 19852, -18118, -15679, -538, -1, 10327, 18606,
-    -8616, 2897, 26277, -1, -1, 6216, -1, 6036 };
+    -8616, 2897, 26277, -1, -1, 6216, -1, 6036
+};
 #elif defined(NF_INT16_INT8) || defined(NF_INT8)
 int8_t b[ARR_LEN] = {
     -40, 12, -70, 65, 102, -89, -41, 46, -40, -47, 37, -103, -51, -56, -119, 20
@@ -39,7 +45,7 @@ int8_t b[ARR_LEN] = {
 int8_t b[ARR_LEN>>1] = { 64, -110, -2, 111, -112, 29, 5, -63 };
 //actual values
 //{ (0, 4), (2, -7), (-2, -1), (-1, 6), (0, -7), (-3, 1), (5, 0), (1, -4) };
-#elif defined(NF_INT16_INT2) || defined(NF_INT8_INT2)
+#elif defined(NF_INT16_INT2) || defined(NF_INT8_INT2) || defined(NF_INT4_INT2) || defined(NF_INT2)
 int8_t b[ARR_LEN>>2] = { -54, 20, -2, -69 };
 // actual values
 // { (-2, -2, 0, -1), (0, 1, 1, 0), (-2, -1, -1, -1), (-1, -2, -1, -2) };
@@ -47,7 +53,7 @@ int8_t b[ARR_LEN>>2] = { -54, 20, -2, -69 };
 _Static_assert(0, "Unsupported number format: b");
 #endif
 
-// function
+// function and reference value
 #ifdef __riscv_xsimd
 #define FUNC_PREFIX _simd_
 #else
@@ -56,18 +62,34 @@ _Static_assert(0, "Unsupported number format: b");
 
 #if defined(NF_INT16)
 #define FUNC_NAME dot_product_int16
+const int32_t ref = -523423903;
 #elif defined(NF_INT8)
 #define FUNC_NAME dot_product_int8
+const int32_t ref = -18060;
 #elif defined(NF_INT4)
 #define FUNC_NAME dot_product_int4
+const int32_t ref = 100;
+#elif defined(NF_INT2)
+#define FUNC_NAME dot_product_int2
+const int32_t ref = 12;
 #elif defined(NF_INT16_INT8)
 #define FUNC_NAME dot_product_int16_int8
+const int32_t ref = 4190439;
 #elif defined(NF_INT16_INT4)
 #define FUNC_NAME dot_product_int16_int4
+const int32_t ref = -240769;
+#elif defined(NF_INT16_INT2)
+#define FUNC_NAME dot_product_int16_int2
+const int32_t ref = -30567;
 #elif defined(NF_INT8_INT4)
 #define FUNC_NAME dot_product_int8_int4
+const int32_t ref = -2028;
 #elif defined(NF_INT8_INT2)
 #define FUNC_NAME dot_product_int8_int2
+const int32_t ref = 404;
+#elif defined(NF_INT4_INT2)
+#define FUNC_NAME dot_product_int4_int2
+const int32_t ref = 4;
 #else
 _Static_assert(0, "Unsupported number format: FUNC_NAME");
 #endif
@@ -76,29 +98,12 @@ _Static_assert(0, "Unsupported number format: FUNC_NAME");
 #define EXPAND_CONCAT(a, b) CONCAT(a, b)
 #define FUNC EXPAND_CONCAT(FUNC_PREFIX, FUNC_NAME)
 
-// reference output
-#if defined(NF_INT16)
-const int32_t ref = -523423903;
-#elif defined(NF_INT8)
-const int32_t ref = -18060;
-#elif defined(NF_INT4)
-const int32_t ref = 100;
-#elif defined(NF_INT16_INT8)
-const int32_t ref = 4190439;
-#elif defined(NF_INT16_INT4)
-const int32_t ref = -240769;
-#elif defined(NF_INT8_INT4)
-const int32_t ref = -2028;
-#elif defined(NF_INT8_INT2)
-const int32_t ref = 404;
-#else
-_Static_assert(0, "Unsupported number format: ref");
-#endif
-
 void main(void) {
     int32_t result;
     PROF_START;
-    for (size_t i = 0; i < LOOPS; i++) { result = FUNC(a, b, ARR_LEN); }
+    for (size_t i = 0; i < LOOPS; i++) {
+        result = FUNC(a, b, ARR_LEN);
+    }
     PROF_STOP;
     //printf("%d\n",result);
     //printf("%d\n",ref);
--- sw/baremetal/vector_dot_product_unaligned_len_even/main.c
+++ sw/baremetal/vector_dot_product_unaligned_len_even/main.c
@@ -6,26 +6,36 @@
 #define LOOPS 1
 #endif
 
-#define ARR_LEN 22
+#define ARR_LEN 24
+// packed data type has to within 1 byte exactly
 
 // input data A
-#if defined(NF_INT16) || defined(NF_INT16_INT8) || defined(NF_INT16_INT4)
+#if defined(NF_INT16) || defined(NF_INT16_INT8) || defined(NF_INT16_INT4) || defined(NF_INT16_INT2)
 int16_t a[ARR_LEN] = {
     29733, 235, 1, -27576, -257, 17289, 10955, -24955,
-    19279, -11328, 144, -12159, 1, -25017, 1, -1540,
-    1, -743, 25778, 21780, -11010, 12645 };
-#elif defined(NF_INT8) || defined(NF_INT8_INT4)
+    19279, -11328, 144, 1, 1, -25017, 10989, -1540,
+    3462, -743, 1, 21780, -11010, 12645, 2962, 18900
+};
+#elif defined(NF_INT8) || defined(NF_INT8_INT4) || defined(NF_INT8_INT2)
 int8_t a[ARR_LEN] = {
     -91, 107, 12, -56, 127, 9, 75, 5,
     -49, 64, 16, 1, 76, -57, 109, 124,
-    6, -103, 50, -108, 126, -27 };
-#elif defined(NF_INT4)
-// packed data type, has to be a multiple of 2 (or padded with 0 at the end)
-int8_t a[ARR_LEN>>1] = { 61, 4, 23, -45, -121, -104, -12, 69, 30, -54, -42 };
+    6, -103, 50, -108, 126, -27, 18, 84
+};
+#elif defined(NF_INT4) || defined(NF_INT4_INT2)
+int8_t a[ARR_LEN>>1] = {
+    61, 4, 23, -45, -121, -104, -12, 69, 30, -54, -42, -54
+};
 /* actual values {
     (-3, 3), (4, 0), (7, 1), (3, -3),
     (7, -8), (-8, -7), (4, -1), (5, 4),
-    (-2, 1), (-6, -4), (6, -3)
+    (-2, 1), (-6, -4), (6, -3), (-6, -4)
+}; */
+#elif defined(NF_INT2)
+int8_t a[ARR_LEN>>2] = { -89, -35, -23, -74, -116, -116 };
+/* actual values {
+    (-1, 1, -2, -2), (1, -1, 1, -1), (1, -2, -2, -1), (-2, 1, -1, -2),
+    (0, -1, 0, -2), (0, -1, 0, -2)
 }; */
 #else
 _Static_assert(0, "Unsupported number format");
@@ -34,20 +44,30 @@ _Static_assert(0, "Unsupported number format");
 // input data B
 #if defined(NF_INT16)
 int16_t b[ARR_LEN] = {
-    2962, 18900, 31627, -24324, -12822, -1, 24477, -9842,
-    16946, -5564, -809, -1, -13335, -15631, -6665, 11742,
-    18272, 1110, 6285, -1, 32137, -1 };
+    31627, -24324, -12822, 4764, 24477, -9842, 16946, -5564,
+    -809, -8233, -13335, -1, -1, 11742, -1, 1110,
+    6285, -1, 32137, 1031, 4415, 19261, -29162, 9529
+};
 #elif defined(NF_INT16_INT8) || defined(NF_INT8)
 int8_t b[ARR_LEN] = {
-    18, 84, 11, 124, 106, 28, 29, 14,
-    -78, -60, 87, 87, 105, 113, 119, 94,
-    -32, -42, 13, 105, 9, -121 };
+    11, 124, 106, 28, 29, 14, -78, -60,
+    87, 87, 105, 113, 119, 94, -32, -42,
+    13, 105, 9, -121, -65, -67, -106, -71
+};
 #elif defined(NF_INT16_INT4) || defined(NF_INT8_INT4) || defined(NF_INT4)
-int8_t b[ARR_LEN>>1] = { -54, 67, 66, 101, -54, -1, -111, 111, -24, 21, -15 };
+int8_t b[ARR_LEN>>1] = {
+    67, 66, 101, -54, -1, -111, 111, -24, 21, -15, 87, 30
+};
 /* actual values {
-    (-6, -4), (3, 4), (2, 4), (5, 6),
-    (-6, -4), (-1, -1), (1, -7), (-1, 6),
-    (-8, -2), (5, 1), (1, -1)
+    (3, 4), (2, 4), (5, 6), (-6, -4),
+    (-1, -1), (1, -7), (-1, 6), (-8, -2),
+    (5, 1), (1, -1), (7, 5), (-2, 1)
+}; */
+#elif defined(NF_INT16_INT2) || defined(NF_INT8_INT2) || defined(NF_INT4_INT2) || defined(NF_INT2)
+int8_t b[ARR_LEN>>2] = { -119, -125, -11, 33, 127, -51 };
+/* actual values {
+    (1, -2, 0, -2), (-1, 0, 0, -2), (1, 1, -1, -1), (1, 0, -2, 0),
+    (-1, -1, -1, 1), (1, -1, 0, -1 )
 }; */
 #else
 _Static_assert(0, "Unsupported number format");
@@ -62,50 +82,64 @@ _Static_assert(0, "Unsupported number format");
 
 #if defined(NF_INT16)
 #define FUNC_NAME dot_product_int16
+const int32_t ref = 884400237;
+const size_t len = 23;
 #elif defined(NF_INT8)
 #define FUNC_NAME dot_product_int8
+const int32_t ref = 2210;
+const size_t len = 23;
 #elif defined(NF_INT4)
 #define FUNC_NAME dot_product_int4
+const int32_t ref = 46;
+const size_t len = 22;
+#elif defined(NF_INT2)
+#define FUNC_NAME dot_product_int2
+const int32_t ref = 3;
+const size_t len = 20;
 #elif defined(NF_INT16_INT8)
 #define FUNC_NAME dot_product_int16_int8
+const int32_t ref = -4583810;
+const size_t len = 23;
 #elif defined(NF_INT16_INT4)
 #define FUNC_NAME dot_product_int16_int4
+const int32_t ref = -145430;
+const size_t len = 22;
+#elif defined(NF_INT16_INT2)
+#define FUNC_NAME dot_product_int16_int2
+const int32_t ref = 139471;
+const size_t len = 20;
 #elif defined(NF_INT8_INT4)
 #define FUNC_NAME dot_product_int8_int4
+const int32_t ref = -538;
+const size_t len = 22;
+#elif defined(NF_INT8_INT2)
+#define FUNC_NAME dot_product_int8_int2
+const int32_t ref = -535;
+const size_t len = 20;
+#elif defined(NF_INT4_INT2)
+#define FUNC_NAME dot_product_int4_int2
+const int32_t ref = 1;
+const size_t len = 20;
 #else
-_Static_assert(0, "Unsupported number format");
+_Static_assert(0, "Unsupported number format: FUNC_NAME");
 #endif
 
 #define CONCAT(a, b) a##b
 #define EXPAND_CONCAT(a, b) CONCAT(a, b)
 #define FUNC EXPAND_CONCAT(FUNC_PREFIX, FUNC_NAME)
 
-// reference output
-#if defined(NF_INT16)
-const int32_t ref = 1850241620;
-#elif defined(NF_INT8)
-const int32_t ref = 41969;
-#elif defined(NF_INT4)
-const int32_t ref = 57;
-#elif defined(NF_INT16_INT8)
-const int32_t ref = -6256095;
-#elif defined(NF_INT16_INT4)
-const int32_t ref = -79927;
-#elif defined(NF_INT8_INT4)
-const int32_t ref = 2209;
-#else
-_Static_assert(0, "Unsupported number format");
-#endif
-
 void main(void) {
+    int32_t result;
+    PROF_START;
     for (size_t i = 0; i < LOOPS; i++) {
-        int32_t result = FUNC(a, b, ARR_LEN);
-        //printf("%d\n",result);
-        //printf("%d\n",ref);
-        if (result != ref) {
-            write_mismatch(result, ref, 1);
-            fail();
-        }
+        result = FUNC(a, b, ARR_LEN);
+    }
+    PROF_STOP;
+    //printf("%d\n",result);
+    //printf("%d\n",ref);
+    if (result != ref) {
+        write_mismatch(result, ref, 1);
+        fail();
     }
     pass();
 }
