8b52e896d4597d2c7dc67aa2c05ff16af54aaa9a
Aleksandar <aleksandarlilic03@gmail.com>
2026-08-13 08:57:57 +0200

    sw(build): error out if both 'MATH_LIB_LOAD_OPT' and 'MATH_LIB_SIMD' are provided in the same build

--- sw/baremetal/Makefile.math_lib_opts.mk
+++ sw/baremetal/Makefile.math_lib_opts.mk
@@ -3,6 +3,11 @@ MATH_LIB_FLAGS ?=
 MATH_LIB_FLAGS_STR =
 
 # select math library flavor
+
+ifeq ("$(strip $(MATH_LIB_LOAD_OPT)) $(strip $(MATH_LIB_SIMD))", "1 1")
+$(error MATH_LIB_LOAD_OPT and MATH_LIB_SIMD cannot be set at the same time)
+endif
+
 ifeq ($(strip $(MATH_LIB_LOAD_OPT)), 1)
 MATH_LIB_FLAGS_STR = SCALAR_LOAD_OPT
 MARCH := rv32im_zicsr_zifencei_zicntr
